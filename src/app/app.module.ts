529c3c8dc2e86d690f4f149779c6a259f524c77d
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-16 10:04:36 -0300

    Initial commit; creating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipeComponent } from "./recipe/recipe.component";
+import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+import { RecipeListComponent } from "./recipe/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "./recipe/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingListEditComponent } from "./shopping-list/shopping-list-edit/shopping-list-edit.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
