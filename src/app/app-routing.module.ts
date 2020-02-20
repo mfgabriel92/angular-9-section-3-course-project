2d6ca38b931e361fa51a58bd0679335545be7eea
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-20 10:05:28 -0300

    Adding Navigation to the App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RecipeComponent },
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
