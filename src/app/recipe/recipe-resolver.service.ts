2db19f800160a80eb8b5d2cbb9539bd4eeb2dddd
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-23 10:03:19 -0300

    Fixing a Bug with the Resolver

--- src/app/recipe/recipe-resolver.service.ts
+++ src/app/recipe/recipe-resolver.service.ts
@@ -7,12 +7,17 @@ import {
 
 import { RecipeApiService } from './recipe-api.service';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]> {
-  constructor(private recipeApi: RecipeApiService) {}
+  constructor(
+    private recipeApi: RecipeApiService,
+    private recipeService: RecipeService
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.recipeApi.fetch();
+    const recipes = this.recipeService.getRecipes();
+    return recipes.length > 0 ? recipes : this.recipeApi.fetch();
   }
 }
