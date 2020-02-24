14d94b8fd09104327499b5aacc6fe99a7a1a00cf
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-24 14:02:27 -0300

    Reflecting the Auth State in the UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 import { RecipeApiService } from './../recipe/recipe-api.service';
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  constructor(private recipeApi: RecipeApiService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private user: Subscription;
+  isLogged: boolean;
 
-  ngOnInit(): void {}
+  constructor(
+    private recipeApi: RecipeApiService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.user.subscribe(user => {
+      this.isLogged = !!user;
+      console.log(this.isLogged);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.user.unsubscribe();
+  }
 
   onSaveClick(): void {
     this.recipeApi.store().subscribe(response => console.log(response));
