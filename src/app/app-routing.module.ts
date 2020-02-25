8684f693d1986008ffd0402609377f5228822468
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-24 22:26:58 -0300

    Adding an Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,14 +9,21 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { EmptyComponent } from './recipe/empty/empty.component';
 import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+import { AuthGuardService } from './auth-guard.service';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full',
+    canActivate: [AuthGuardService]
+  },
   {
     path: 'recipes',
     component: RecipeComponent,
+    canActivate: [AuthGuardService],
     children: [
       { path: '', component: EmptyComponent },
       { path: 'new', component: RecipeEditComponent },
@@ -32,7 +39,11 @@ const routes: Routes = [
       }
     ]
   },
-  { path: 'shopping-list', component: ShoppingListComponent }
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent,
+    canActivate: [AuthGuardService]
+  }
 ];
 
 @NgModule({
---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  UrlTree
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { map, take } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuardService implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.user.pipe(
+      take(1),
+      map(user => {
+        if (user) {
+          return true;
+        }
+
+        return this.router.createUrlTree(['/signin']);
+      })
+    );
+  }
+}
