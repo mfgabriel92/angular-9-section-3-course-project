bbd3cfd0ec8d146355b70bb60553401cd2353ff0
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-27 19:47:12 -0300

    Setting Up Auth Reducer & Actions

---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export class Login implements Action {
+  readonly type = LOGIN;
+
+  constructor(
+    public payload: {
+      id: string;
+      email: string;
+      token: string;
+      expiresIn: Date;
+    }
+  ) {}
+}
+
+export class Logout implements Action {
+  readonly type = LOGOUT;
+}
+
+export type AuthActions = Login | Logout;
