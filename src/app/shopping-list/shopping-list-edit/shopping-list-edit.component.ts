825642af7e4cd6e98ffa373289f0e329fc8e36a9
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-27 15:59:39 -0300

    First Summary & Clean Up

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -3,8 +3,6 @@ import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
-// import { ShoppingListService } from '../shopping-list.service';
-import { Subscription } from 'rxjs/internal/Subscription';
 
 import * as ShoppingListActions from '../store/shopping-list.actions';
 import * as fromShoppingList from '../store/shopping-list.reducer';
